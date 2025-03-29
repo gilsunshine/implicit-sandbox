c532bd7bb62c0ee0d6bde1940a34b270b07adf84
Gil Sunshine <32219998+gilsunshine@users.noreply.github.com>
2025-03-28 20:03:27 -0700

    Updates

---
+++ .vite/deps/three_examples_jsm_objects_MarchingCubes__js.js
@@ -0,0 +1,12 @@
+import {
+  MarchingCubes,
+  edgeTable,
+  triTable
+} from "./chunk-XM6KJDFG.js";
+import "./chunk-VCVPXQVX.js";
+import "./chunk-5FUTL2UF.js";
+export {
+  MarchingCubes,
+  edgeTable,
+  triTable
+};
---
+++ .vite/deps/ws-RZIMPEVM.js
@@ -0,0 +1,18 @@
+import {
+  __commonJS
+} from "./chunk-5FUTL2UF.js";
+
+// browser-external:ws
+var require_ws = __commonJS({
+  "browser-external:ws"(exports, module) {
+    module.exports = Object.create(new Proxy({}, {
+      get(_, key) {
+        if (key !== "__esModule" && key !== "__proto__" && key !== "constructor" && key !== "splice") {
+          console.warn(`Module "ws" has been externalized for browser compatibility. Cannot access "ws.${key}" in client code. See https://vite.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility for more details.`);
+        }
+      }
+    }));
+  }
+});
+export default require_ws();
+//# sourceMappingURL=ws-RZIMPEVM.js.map
